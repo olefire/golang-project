9bbcf836770a15e1a100d3b17acbf5c9a6d3f4e8
olefire <olefirenkozahar@yandex.ru>
2023-12-10 01:31:08 +0300

    chore: get rid of useless components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,14 @@ import './App.css';
 // import axios from 'axios'
 // import {useEffect, useState} from "react";
 import {Routes, Route, BrowserRouter} from "react-router-dom";
-import {Homepage} from "./views/homepage";
-import {Batch} from "./views/batch";
 import {PastePage} from "./views/paste";
-import {TestLintPage} from "./views/testLintPage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="main" element={<Homepage/>}/>
-                <Route path="batch" element={<Batch/>}/>
+                <Route path="" element={<PastePage/>}/>
                 <Route path="paste/:id" element={<PastePage/>}/>
-                <Route path="lint" element={<TestLintPage/>}/>
             </Routes>
         </BrowserRouter>
     );
